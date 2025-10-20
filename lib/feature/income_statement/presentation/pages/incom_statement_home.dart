34114c76d1b23cc082e30bee285b415826a1441e
Edward Cristobal Diaz Valda <edward.cristob@gmail.com>
2025-10-20 18:59:40 -0400

    feat(income_statement): add IncomStatementHome widget for income statement feature

---
+++ lib/feature/income_statement/presentation/pages/incom_statement_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class IncomStatementHome extends StatefulWidget {
+  const IncomStatementHome({super.key});
+
+  @override
+  State<IncomStatementHome> createState() => _IncomStatementHomeState();
+}
+
+class _IncomStatementHomeState extends State<IncomStatementHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
